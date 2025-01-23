3ec74aa7f3cfe544412228fd19519d94f8b4e739
Andrey Lebedev <a.lebedev@severnaya.ru>
2025-01-23 08:50:38 +0300

    add migration HL "Тестовый HL"

---
+++ local/php_interface/migrations/Version20250123085003.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version20250123085003 extends Version
+{
+    protected $author = "admin";
+
+    protected $description = "Новый HL \"Тестовый HL\"";
+
+    protected $moduleVersion = "4.18.0";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+    $hlblockId = $helper->Hlblock()->saveHlblock(array (
+  'NAME' => 'Test',
+  'TABLE_NAME' => 'test_db',
+  'LANG' => 
+  array (
+    'ru' => 
+    array (
+      'NAME' => 'Тестовый HL',
+    ),
+  ),
+));
+        $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_NAME',
+  'USER_TYPE_ID' => 'string',
+  'XML_ID' => '',
+  'SORT' => '100',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'N',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'ROWS' => 1,
+    'REGEXP' => '',
+    'MIN_LENGTH' => 0,
+    'MAX_LENGTH' => 0,
+    'DEFAULT_VALUE' => '',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'Имя',
+    'ru' => 'Имя',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => 'Имя',
+    'ru' => 'Имя',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => 'Имя',
+    'ru' => 'Имя',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+            $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_EMAIL',
+  'USER_TYPE_ID' => 'string',
+  'XML_ID' => '',
+  'SORT' => '100',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'Y',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'ROWS' => 1,
+    'REGEXP' => '',
+    'MIN_LENGTH' => 0,
+    'MAX_LENGTH' => 0,
+    'DEFAULT_VALUE' => '',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'Email',
+    'ru' => 'Email',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => 'Email',
+    'ru' => 'Email',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => 'Email',
+    'ru' => 'Email',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+            $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_PHONE',
+  'USER_TYPE_ID' => 'integer',
+  'XML_ID' => '',
+  'SORT' => '100',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'Y',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'MIN_VALUE' => 0,
+    'MAX_VALUE' => 0,
+    'DEFAULT_VALUE' => NULL,
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'Телефон',
+    'ru' => 'Телефон',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => 'Телефон',
+    'ru' => 'Телефон',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => 'Телефон',
+    'ru' => 'Телефон',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+        }
+}
